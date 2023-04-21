576cf3c440d6c1f338464a8873061e9af0920505
lamtota40 <lamtota40@gmail.com>
2023-04-21 09:42:56 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -13,6 +13,16 @@ LIGHT='\033[0;37m'
 # =====================================================
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
+
+mkdir /etc/xray
+mkdir /var/lib/crot;
+echo "IP=" >> /var/lib/crot/ipvps.conf
+echo "Masukkan Domain Anda, Jika Anda Tidak Memiliki Domain Klik Enter"
+echo "Ketikkan Perintah newhost setelah proses instalasi Script Selesai"
+read -p "Hostname / Domain: " host
+echo "IP="$host >> /var/lib/crot/ipvps.conf
+echo "$host" >> /etc/xray/domain
+
 domain=$(cat /etc/xray/domain)
 apt install iptables iptables-persistent -y
 apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
